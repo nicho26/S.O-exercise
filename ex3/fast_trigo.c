a4fbf95c99bcf00f02d7ebaf8381934eba2ac3a1
nicho26 <97127494+nicho26@users.noreply.github.com>
2025-03-05 11:13:27 +0100

    Add files via upload
    exercise 28/2/25

---
+++ ex3/fast_trigo.c
@@ -0,0 +1,13 @@
+#include "fast_trigo.h"
+
+
+// ERROR: Functions are redefined here!
+float fast_sin(float x) {
+    return sinf(x);
+}
+
+float fast_cos(float x) {
+    return cosf(x);
+}
+
+
---
+++ ex3/fast_trigo.h
@@ -0,0 +1,10 @@
+// No include guards! Will cause multiple inclusion errors.
+#ifndef FAST_SIN_H
+#define FAST_SIN_H
+
+#include <math.h>
+
+float fast_sin(float x);
+float fast_cos(float x);
+
+#endif
